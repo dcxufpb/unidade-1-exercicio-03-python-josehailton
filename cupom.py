e9005167f86bb0f46d3aee84e36b310d3823662e
josehailton <71135811+josehailton@users.noreply.github.com>
2020-09-21 14:02:55 -0300

    Update cupom.py
    Transformei em uma string só na hora de retornar.

--- cupom.py
+++ cupom.py
@@ -13,4 +13,12 @@ inscricao_estadual = "244.898.500.113"
 
 def imprime_dados_loja():
     # Implemente aqui
-    return "" 
+    nome_loja
+    s01 = ("%s, %d %s" % (logradouro, numero, complemento))
+    s02 = ("%s - %s - %s" % (bairro, municipio, estado))
+    s03 = ("CEP:%s Tel %s" % (cep, telefone))
+    observacao
+    s04 = ("CNPJ: %s" % cnpj)
+    s05 = ("IE: %s" % inscricao_estadual)
+    stringResultado = "{}\n{}\n{}\n{}\n{}\n{}".format(nome_loja, s01, s02, s03, observacao, s04, s05)
+    return print(stringResultado) 
